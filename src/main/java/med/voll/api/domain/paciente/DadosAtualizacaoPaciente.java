4d36279fe10a0d96201c3c579569bd719dc3d15e
Fábio Matos <gleek.rj@gmail.com>
2024-09-26 17:52:46 -0300

    Atualização projeto - CRUD de Médicos e Pacientes Inclusão de melhores práticas para chamada e retorno de chamadas http e implementação da parte de segurança usando JWT.

--- src/main/java/med/voll/api/domain/paciente/DadosAtualizacaoPaciente.java
+++ src/main/java/med/voll/api/domain/paciente/DadosAtualizacaoPaciente.java
@@ -1,12 +1,16 @@
-package med.voll.api.paciente;
+package med.voll.api.domain.paciente;
 
 import jakarta.validation.constraints.NotNull;
-import med.voll.api.endereco.DadosEndereco;
+import med.voll.api.domain.endereco.DadosEndereco;
 
 public record DadosAtualizacaoPaciente(
-        @NotNull Long id,
+        @NotNull(message = "{id.obrigatoria}")
+        Long id,
+
         String nome,
+
         String telefone,
+
         DadosEndereco endereco
 ) {
 }
--- src/main/java/med/voll/api/domain/paciente/Paciente.java
+++ src/main/java/med/voll/api/domain/paciente/Paciente.java
@@ -1,19 +1,19 @@
-package med.voll.api.medico;
+package med.voll.api.domain.paciente;
 
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import med.voll.api.endereco.Endereco;
+import med.voll.api.domain.endereco.Endereco;
 
-@Table(name = "medicos")
-@Entity(name = "Medico")
+@Table(name = "pacientes")
+@Entity(name = "Paciente")
 @Getter
 @NoArgsConstructor
 @AllArgsConstructor
 @EqualsAndHashCode(of = "id")
-public class Medico {
+public class Paciente {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -21,45 +21,36 @@ public class Medico {
     private String nome;
     private String email;
     private String telefone;
-    private String crm;
-
-    @Enumerated(EnumType.STRING)
-    private Especialidade especialidade;
-
+    private String cpf;
     @Embedded
     private Endereco endereco;
 
     private Boolean ativo;
 
-    public Medico(DadosCadastroMedico dados) {
+    public Paciente(DadosCadastroPaciente dados) {
         this.nome = dados.nome();
         this.email = dados.email();
         this.telefone = dados.telefone();
-        this.crm = dados.crm();
-        this.especialidade = dados.especialidade();
+        this.cpf = dados.cpf();
         this.endereco = new Endereco(dados.endereco());
         this.ativo = true;
     }
 
-    public Medico(DadosAtualizacaoMedico dados) {
-        this.nome = dados.nome();
-        this.telefone = dados.telefone();
-        this.endereco = new Endereco(dados.endereco());
-    }
-
-    public void atualizarInformacoes(DadosAtualizacaoMedico dados) {
-        if (dados.nome() != null){
+    public void atualizarInformacoes(DadosAtualizacaoPaciente dados) {
+        if (dados.nome() != null) {
             this.nome = dados.nome();
         }
+
         if (dados.telefone() != null){
             this.telefone = dados.telefone();
         }
+
         if (dados.endereco() != null){
             this.endereco.atualizarInformacoes(dados.endereco());
         }
     }
 
-    public void excluir() {
+    public void inativar() {
         this.ativo = false;
     }
 }
